e391af75497b104b117cfe000b9cb1f2ebdfee02
Peter Richmond <peter.richmond@bbc.co.uk>
2024-07-24 13:34:02 +0100

    fix

--- love_api.lua
+++ love_api.lua
@@ -2,7 +2,9 @@ local path = (...):match('(.-)[^%./]+$')
 
 return {
     version = '11.5',
-    build-date = os.date("!%Y-%m-%dT%H:%M:%SZ"),
+    build = {
+        date = os.date("!%Y-%m-%dT%H:%M:%SZ"),
+    }
     functions = {
         {
             name = 'getVersion',
