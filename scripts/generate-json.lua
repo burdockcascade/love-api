7d838a76abc5efab69e8043e93e0a8c95c8c62a1
Peter Richmond <peter.richmond@bbc.co.uk>
2024-07-24 12:19:50 +0100

    add artifact

--- scripts/generate-json.lua
+++ scripts/generate-json.lua
@@ -1,5 +1,5 @@
 local dkjson = require('dkjson')
 local api = require("love_api")
 
-local f = assert(io.open("../love-api.json", "wb"))
+local f = assert(io.open("love-api.json", "wb"))
 f:write(dkjson.encode(api, {indent = true, }))
