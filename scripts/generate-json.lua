c15aea8582ca46b253893c2cafd670a093ac466a
Peter Richmond <peter.richmond@bbc.co.uk>
2024-07-24 12:31:43 +0100

    wip

--- scripts/generate-json.lua
+++ scripts/generate-json.lua
@@ -2,4 +2,4 @@ local dkjson = require('dkjson')
 local api = require("love_api")
 
 local f = assert(io.open("love-api.json", "wb"))
-f:write(dkjson.encode(api, {indent = true, sort-keys = true}))
+f:write(dkjson.encode(api, {indent = true}))
